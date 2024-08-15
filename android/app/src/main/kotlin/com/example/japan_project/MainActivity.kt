3d8fbb81e284d9227a526028be203240d6b6a4d0
Abdullah <abdulloibrohimov@gmail.com>
2024-08-15 14:33:06 +0500

    starting the project

---
+++ android/app/src/main/kotlin/com/example/japan_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.japan_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+
+/// just getting started
+
+void main(){
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
